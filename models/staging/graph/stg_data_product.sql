509784a305fa6589e94f597f14dfdf72b5b553ce
Dieter <dieter.de.beuckeleer@velux.com>
2026-02-14 21:33:50 +0100

    bugfix

--- models/staging/graph/stg_data_product.sql
+++ models/staging/graph/stg_data_product.sql
@@ -15,7 +15,7 @@ with data_products_cte as (
         meta,
         split(file_path, '/') as path_array,
         array_slice(path_array,0,array_size(path_array) - 1) as folder_path_array,
-        array_to_string(folder_path_array, '/')::varchar as folder_path
+        array_to_string(folder_path_array, '/')::varchar as folder_path,
         path_array[array_size(path_array) - 2]::varchar as folder_name,
     
         parse_json(meta):featured_models as featured_models,
