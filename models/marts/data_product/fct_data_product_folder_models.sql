a23d581bd78bb22fda3999ea6f33d06e0b32c435
Dieter <dieter.de.beuckeleer@velux.com>
2026-02-16 15:08:18 +0100

    bugfix

--- models/marts/data_product/fct_data_product_folder_models.sql
+++ models/marts/data_product/fct_data_product_folder_models.sql
@@ -2,7 +2,7 @@ select
 
     node_unique_id as model,
     node_folder_path as model_folder_path,
-    data_product_folder_name as change_folder_path_to
+    data_product_folder_path as change_folder_path_to
 
 from {{ ref('int_data_product_relationships') }}
-where data_product_folder_name <> node_folder_path
+where data_product_folder_path <> node_folder_path
